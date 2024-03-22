12a4d4ee9605ddaa807366c6018acd0b3f6f4c8e
Yussuf <you@example.com>
2024-03-22 10:45:23 +0100

    sell

--- src/components/HomeComponents/HeroComponents/Hero.jsx
+++ src/components/HomeComponents/HeroComponents/Hero.jsx
@@ -5,35 +5,37 @@ const Hero = () => {
   return (
     <section
       className=" w-full min-h-screen  text-center md:text-left  px-12 md:px-32  text-white poppins-regular 
-       bg-[#0E0C4D]   py-20 md:py-40 
+       bg-[#0E0C4D]  py-8
   "
     >
-      <div className="flex flex-col md:flex-row items-center justify-between w-full  gap-12 md:gap-8 ">
-        <div className="md:flex-1  ">
-          <h2 className="text-white poppins-medium text-3xl   md:text-[40px]  leading-[50px] mb-8   md:w-[90%]">
-            Sell your <span className="text-[#00FFFC]">smartphone</span> faster
-            and conveniently
-          </h2>
-          <p className="poppins-medium md:text-[20px] md:w-[90%]">
-            Fairshop is the easiest place to sell your smartphone and receive
-            payment instantly
-          </p>
+      <div className="flex flex-col h-screen gap-8 justify-center ">
+        <div className="flex flex-col md:flex-row items-center justify-between w-full  gap-12 md:gap-8 ">
+          <div className="md:flex-1  ">
+            <h2 className="text-white poppins-medium text-3xl   md:text-[40px]  leading-[50px] mb-8   md:w-[90%]">
+              Sell your <span className="text-[#00FFFC]">smartphone</span>{" "}
+              faster and conveniently
+            </h2>
+            <p className="poppins-medium md:text-[20px] md:w-[90%]">
+              Fairshop is the easiest place to sell your smartphone and receive
+              payment instantly
+            </p>
+          </div>
+          <div className="flex-1 md:flex justify-end">
+            <img
+              src={heroImg}
+              alt=""
+              className="md:min-w-[300px] md:min-h-[250px] max-h-[400px] md:max-h-[350px] md:max-w-[500px]"
+            />
+          </div>
         </div>
-        <div className="flex-1 md:flex justify-end">
-          <img
-            src={heroImg}
-            alt=""
-            className="md:min-w-[300px] md:min-h-[300px] max-h-[400px] md:max-h-[500px] "
-          />
-        </div>
-      </div>
 
-      <div className="text-center  my-12">
-        <Button className="bg-white bg-opacity-10 hover:bg-white hover:bg-opacity-10 rounded-2xl">
-          <a href="#sell" className=" flex items-center">
-            Sell Now <Plus className="h-4 w-4 ml-2" />
-          </a>
-        </Button>
+        <div className="text-center ">
+          <Button className="bg-white bg-opacity-10 hover:bg-white hover:bg-opacity-10 rounded-2xl">
+            <a href="#sell" className=" flex items-center">
+              Sell Now <Plus className="h-4 w-4 ml-2" />
+            </a>
+          </Button>
+        </div>
       </div>
     </section>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 import Brands from "../components/HomeComponents/BrandComponents/Brands";
 import HowItWorks from "../components/HomeComponents/HowItWorksComponents/HowItWorks";
@@ -8,8 +8,6 @@ import FAQs from "../components/HomeComponents/FAQsComponents/FAQs";
 import Hero from "../components/HomeComponents/HeroComponents/Hero";
 
 const Home = () => {
-  const [stepContent, setStepContent] = useState(1);
-
   const goToTop = () => {
     window.scrollTo({
       top: 0,
@@ -20,8 +18,10 @@ const Home = () => {
 
   return (
     <main className="w-full h-full relative ">
-      <section className="pt-12 md:pt-0">
-        <Hero />
+      <section className="">
+        <div className="">
+          <Hero />
+        </div>
         <section id="sell" className="relative bg-white mb-28 ">
           <div className="h-[80px] bg-white rounded-t-2xl absolute left-[2%] top-[-40px] w-[95%] mx-auto z-10"></div>
           <Brands />
